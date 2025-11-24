9ad5f768ed0a2540b1e9bd75c556d7751bd3e060
MishanyaX <michasergeev@yandex.ru>
2025-11-25 02:00:41 +0300

    Убрали обрботку ( в функции infix_to_postfix

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ def infix_to_postfix(expression):
     for token in tokens:
         if token.isdigit():
             output.append(token)
-        elif token == '(':
-            stack.append(token)
         elif token == ')':
             while stack and stack[-1] != '(':
                 output.append(stack.pop())
